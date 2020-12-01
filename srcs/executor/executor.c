dc79ad3ca326d04b9db71c159cacd185216ca0f4
anatashi <anatashi@student.21-school.ru>
2020-12-01 18:16:38 +0300

    for vbox

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 12:38:26 by anatashi          #+#    #+#             */
-/*   Updated: 2020/11/26 18:09:32 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/12/01 18:16:35 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,29 +69,53 @@ void			give_env(t_data *data)
 }
 #endif
 
+int			check_syntax_error(t_commands *redir)
+{
+	char	*token_err;
+
+	if (redir->redir->type_redir != 3)
+	{
+		if (!redir->redir->cmd && redir->redir->pipe->cmd)
+		{
+			token_err = ft_strdup("`|'\n");
+			error_case("minishell: syntax error near unexpected token ", token_err, NULL);
+			ft_free_tmp(token_err);
+			return (1);
+		}
+	}
+	return (0);
+}
+
 void			selection_cmd(t_commands *cmd, t_data *data,
 								t_commands *redirect, t_commands *pip)
 {
 	// #if 0
 	(void)pip;
 	// #endif
+
 	if (redirects(redirect))
+	{
+		// check_syntax_error(redirect);
 		return;
+	}
+	// if (check_syntax_error(redirect))
+		// return;
 	if (cmd->num_cmd == CMD_PWD)
 		ft_pwd(cmd, data);
 	else if (cmd->num_cmd == CMD_ENV)
-		// give_env(data);
 		ft_env(cmd, data);
 	else if (cmd->num_cmd == CMD_CD)
 		ft_cd(&cmd, data);
 	else if (cmd->num_cmd == CMD_ECHO)
 		ft_echo(cmd);
 	else if (cmd->num_cmd == CMD_EXPORT)
-		ft_export(&cmd, &data);
+		ft_export(&cmd, &data, -1);
 	else if (cmd->num_cmd == CMD_UNSET)
 		ft_unset(&cmd, &data);
 	else if (cmd->num_cmd == CMD_IN_PATH)
-		ft_check_cmd_in_path(&cmd, &data);	
+		ft_check_cmd_in_path(&cmd, &data);
+	if (cmd->num_cmd == CMD_ERROR)
+		error_case("minishell: ", cmd->cmd, ": command not found\n");
 }
 
 void			execute_cmd_line(t_commands *cmd, t_data *data)
@@ -100,7 +124,7 @@ void			execute_cmd_line(t_commands *cmd, t_data *data)
 	t_commands	*pip;
 	t_commands	*redirect;
 
-	if (cmd->cmd)
+	if (cmd->cmd || cmd->type_redir)
 	{
 		pip = cmd;
 		redirect = cmd;
@@ -116,9 +140,9 @@ void			execute_cmd_line(t_commands *cmd, t_data *data)
 
 void	executor(t_commands *syntax_tree, t_data *data)
 {
-	if (syntax_tree && syntax_tree->invalid != CMD_ERROR)
+	if (syntax_tree)
 	{
-		while (syntax_tree && syntax_tree->invalid != CMD_ERROR)
+		while (syntax_tree)
 		{
 			execute_cmd_line(syntax_tree, data);
 			syntax_tree = syntax_tree->next;
@@ -126,11 +150,11 @@ void	executor(t_commands *syntax_tree, t_data *data)
 			dup2(data->fd_0, 0);
 		}
 	}
-	else
-	{
-		ft_putendl("Invalid command");
-		ft_free_tmp(data->error_string);
-		if (!(data->error_string =ft_strdup("127")))
-			error_output(syntax_tree, data, NULL);
-	}	
+	// else
+	// {
+	// 	ft_putendl("Invalid command");
+	// 	ft_free_tmp(data->error_string);
+	// 	if (!(data->error_string =ft_strdup("127")))
+	// 		error_output(syntax_tree, data, NULL);
+	// }	
 }
--- srcs/parser/init.c
+++ srcs/parser/init.c
@@ -6,7 +6,7 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 18:25:17 by anatashi          #+#    #+#             */
-/*   Updated: 2020/11/24 19:25:03 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/12/01 18:16:34 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,32 +22,67 @@ static void		init_arg(t_commands *cmd)
 	cmd->count_args = ft_lstsize(cmd->lst);
 }
 
-static	int		checking_cmd_for_tocken(char *word)
+int			check_cmd_in_path(t_commands *cmd, char *tmp, char *path)
 {
-	if (!(ft_strcmp("echo", word)))
-		return (CMD_ECHO);
-	else if (!(ft_strcmp("cd", word)))
-		return (CMD_CD);
-	else if (!(ft_strcmp("pwd", word)))
-		return (CMD_PWD);
-	else if (!(ft_strcmp("export", word)))
-		return (CMD_EXPORT);
-	else if (!(ft_strcmp("unset", word)))
-		return (CMD_UNSET);
-	else if (!(ft_strcmp("env", word)))
-		return (CMD_ENV);
-	else if (!(ft_strcmp("exit", word)))
-		return (CMD_EXIT);
-	return (CMD_IN_PATH);
+	DIR		*dir;
+	struct	dirent *entry;
+	int		i;
+	int		j;
+	char	**split_path;
+	
+	i = -1;
+	split_path = ft_split(path + 5, ':');
+	while (split_path[++i])
+	{
+		dir = opendir(split_path[i]);
+		while ((entry = readdir(dir)))
+		{
+			if (!ft_strcmp(entry->d_name, tmp))
+			{
+				j = i + 1;
+				cmd->cmd = ft_strjoin("/",cmd->cmd);
+				cmd->cmd_dir = split_path[i--];
+				while (i)
+					ft_free_tmp(split_path[i--]);
+				while (split_path[j])
+					ft_free_tmp(split_path[j++]);
+				free(split_path);
+				return (0);
+			}
+		}
+	}
+	ft_free_two_dimensional_arr(split_path);
+	return (1);
 }
 
-static int		checking_cmd(char *cmd)
+static	int		checking_cmd_for_tocken(t_commands *cmd, char *tmp, char *path)
+{
+	if (!(ft_strcmp("echo", tmp)))
+		return (CMD_ECHO);
+	else if (!(ft_strcmp("cd", tmp)))
+		return (CMD_CD);
+	else if (!(ft_strcmp("pwd", tmp)))
+		return (CMD_PWD);
+	else if (!(ft_strcmp("export", tmp)))
+		return (CMD_EXPORT);
+	else if (!(ft_strcmp("unset", tmp)))
+		return (CMD_UNSET);
+	else if (!(ft_strcmp("env", tmp)))
+		return (CMD_ENV);
+	else if (!(ft_strcmp("exit", tmp)))
+		return (CMD_EXIT);
+	else if (!check_cmd_in_path(cmd, tmp, path))
+		return (CMD_IN_PATH);
+	return(CMD_ERROR);
+}
+
+static int		checking_cmd(t_commands *cmd, char *path)
 {
 	char 		*tmp;
 	size_t		i;
 
 	i = -1;
-	tmp = cmd;
+	tmp = cmd->cmd;
 	while (tmp[++i])
 	{
 		if (ft_isalpha(tmp[i]))
@@ -56,20 +91,20 @@ static int		checking_cmd(char *cmd)
 	}
 	if (tmp[i])
 		return (-1);
-	return (checking_cmd_for_tocken(tmp));
+	return (checking_cmd_for_tocken(cmd, tmp, path));
 }
 
-static	void	init_cmd(t_commands *cmd)
+static	void	init_cmd(t_commands *cmd, t_data *data)
 {
 	cmd->cmd = cmd->lst->content;
-	cmd->invalid = checking_cmd(cmd->cmd);
+	cmd->invalid = checking_cmd(cmd, data->envp[data->path]);
 	cmd->num_cmd = cmd->invalid;
 }
 
-void			init(t_commands **cmd)
+void			init(t_commands **cmd, t_data *data)
 {
 	if (!(*cmd)->lst)
 		return;
-	init_cmd(*cmd);
+	init_cmd(*cmd, data);
 	init_arg(*cmd);
 }
