7cb8a111909f8abf8615e7be323712f99d989432
anatashi <anatashi@student.21-school.ru>
2020-12-01 18:00:37 +0300

    for vbox

--- srcs/temp/minishell.c
+++ srcs/temp/minishell.c
@@ -6,7 +6,7 @@
 /*   By: anatashi <anatashi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 15:00:57 by anatashi          #+#    #+#             */
-/*   Updated: 2020/11/26 18:56:24 by anatashi         ###   ########.fr       */
+/*   Updated: 2020/12/01 18:00:29 by anatashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int    				main(int argc, char **argv, char **envp)
 	/*
 	**	for minishell_test
 	*/
-	#if 0
+	// #if 0
 	if (argv[1][0] == '-' && argv[1][1] == 'c')
 	{
 		lexer_build(ft_strdup(argv[2]), ft_strlen(argv[2]), &lexerbuf);
@@ -43,7 +43,7 @@ int    				main(int argc, char **argv, char **envp)
 		executor(syntax_tree, data);
 	}
 	else
-	#endif
+	// #endif
 	{
 		while (1)
 		{
